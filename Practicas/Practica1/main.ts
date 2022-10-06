77de160c6f26d584611a72b0153c4034c70e46c0
David G.Muñoz <72562392+davidgregoriom@users.noreply.github.com>
2022-10-06 21:35:56 +0200

    Create main.ts

---
+++ Practicas/Practica1/main.ts
@@ -0,0 +1,26 @@
+export function add(a: number, b: number): number {
+    return a + b;
+  }
+  
+  const url=new URL(import.meta.url);
+  if()
+
+
+
+
+  // Learn more at https://deno.land/manual/examples/module_metadata#concepts
+  if (import.meta.main) {
+    console.log("Add 2 + 3 =", add(2, 3));
+  }
+
+
+
+
+
+/*
+  interface ImportMeta{
+    main: boolean;
+    url: string;
+    resolve(specifier: string):string;
+}
+
