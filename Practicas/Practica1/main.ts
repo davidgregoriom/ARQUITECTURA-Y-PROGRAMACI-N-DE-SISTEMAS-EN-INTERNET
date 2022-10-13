e34c785e5fc1a51a6576fbfe75fe28b22bb47d23
David G.Muñoz <72562392+davidgregoriom@users.noreply.github.com>
2022-10-13 19:46:58 +0200

    Update main.ts

--- Practicas/Practica1/main.ts
+++ Practicas/Practica1/main.ts
@@ -1,42 +1,47 @@
-	type  JSONObject = {
-		[key: string]: string | JSONObject
-	}
+type  JSONObject = {
+	[key: string]: string | JSONObject
+}
 
-	const  obj: JSONObject =
-	{
-		"cookies-modal": {
-			"refuse":  "Rechazar",
-			"title":  "Aviso de Cookies",
-			"text":  "Este sitio web utiliza cookies propias y de terceros"
+const  obj: JSONObject =
+{
+	"cookies-modal": {
+		"refuse":  "Rechazar",
+		"title":  "Aviso de Cookies",
+		"text":  "Este sitio web utiliza cookies propias y de terceros"
+	},
+	"forgot-password": {
+		"send-button":  "Enviar",
+		"email": {
+			"label":  "Correo electrónico",
+			"placeholder":  "Correo electrónico",
+			"error":  "No existe una cuenta de usuario asociada a este correo electrónico"
 		},
-		"forgot-password": {
-			"send-button":  "Enviar",
-			"email": {
-				"label":  "Correo electrónico",
-				"placeholder":  "Correo electrónico",
-				"error":  "No existe una cuenta de usuario asociada a este correo electrónico"
-			},
-			"sent-modal": {
-				"send-again-button":  "Volver a enviar email",
-				"text":  "Revisa la bandeja de tu correo electrónico, si no se encuentra ahí, es posible que esté en la carpeta de Spam.",
-				"title":  "Email enviado"
-			},
-			"text":  "Escribe tu correo electrónico y te enviaremos un email con un enlace para que puedas crear una nueva contraseña.",
-			"title":  "Contraseña olvidada"
+		"sent-modal": {
+			"send-again-button":  "Volver a enviar email",
+			"text":  "Revisa la bandeja de tu correo electrónico, si no se encuentra ahí, es posible que esté en la carpeta de Spam.",
+			"title":  "Email enviado"
 		},
-	}
-	const convert = (obj: JSONObject): JSONObject=> {	
-		if (typeof obj === "object") {
-			const ret = Object.keys(obj).reduce((acc:Array<string>, key: string) => {
-				if(typeof obj[key] === "string"){
-					obj.key===acc+obj[key];
-					return obj.keys;
-				}	
-					acc.push(key);
-					return acc;			
-			}, {})
-		}
-		return obj;
-	};
+		"text":  "Escribe tu correo electrónico y te enviaremos un email con un enlace para que puedas crear una nueva contraseña.",
+		"title":  "Contraseña olvidada"
+	},
+}
 
-	console.log(convert(obj));
+
+const convert = (obj: JSONObject): JSONObject => {
+  const newObj: JSONObject = {};
+  const recursive = (obj: JSONObject, path: string = '') => {
+	Object.keys(obj).forEach(key =>{
+      const value = obj[key];
+      if (typeof value === 'object') {
+        recursive(value, path + key + '.');
+      } else {
+        newObj[path + key] = value;
+      }
+    })
+  };
+  recursive(obj);
+  return newObj;
+}
+
+console.log(convert(obj));
+  
